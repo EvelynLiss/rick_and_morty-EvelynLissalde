c4464732f4775260b8929cb2aafb5fe59a9ca0e5
Evelyn Lissalde <eveviki98@gmail.com>
2023-04-21 19:06:10 -0300

    rick and morty

---
+++ Client/src/components/Form/Form.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import validation from "../Validation/Validation";
+
+const Form = ({ login }) => {
+    const [errors, setErrors] = useState({})
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    });
+    const handleChange = (event) => {
+        setUserData({
+            ...userData, 
+            [event.target.name]: event.target.value
+          
+        })
+
+        setErrors(validation({
+            ...userData,
+            [event.target.name]: event.target.value
+        }))
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        login(userData);
+
+    }
+
+
+    return(
+       
+        <form onSubmit={handleSubmit}>
+        
+            <label htmlFor="email"> </label>
+            <input type="text" name='email' value={userData.email} onChange={handleChange} style={{background: 'green', color: 'black', fontSize: '20px'}} />
+            {errors.email && <p>{errors.email}</p>}
+            <label htmlFor="password">Password: </label>
+            <input type="text" name='password' value={userData.password} onChange={handleChange} style={{background: 'green', color: 'black', fontSize: '20px'}}/>
+            {errors.password && <p>{errors.password}</p>}
+
+            <button>Submit</button>
+
+        </form>
+    )
+}
+
+export default Form;
